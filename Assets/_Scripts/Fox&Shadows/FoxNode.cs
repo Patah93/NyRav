c12cf9a063513da1a3954fbd46d8e7b2060f63c6
Patah93 <hedenstrom.patrik@gmail.com>
2014-04-29 11:57:46 +0200

    Node

--- Assets/_Scripts/Fox&Shadows/FoxNode.cs
+++ Assets/_Scripts/Fox&Shadows/FoxNode.cs
@@ -4,15 +4,23 @@ using System.Collections;
 public class FoxNode : MonoBehaviour {
 
 	public FoxNode _nextNode;
-
+	public FoxNode _prevNode;
 
 	// Use this for initialization
 	void Start () {
-	
+		if (_nextNode != null) {
+			_nextNode.setPrevNode (this);
+		}
+
+		GetComponent<Renderer>().enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void setPrevNode (FoxNode prevNode){
+		_prevNode = prevNode;
 	}
 }
