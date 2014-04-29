968681e4e1652480e4600b81836b079aa6205b97
Jessica Lee <jessicall_2@msn.com>
2014-04-29 12:03:54 +0200

    derp

---
+++ Assets/_Scripts/KillThrowObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillThrowObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision deadthing){
+
+		if(deadthing.gameObject.tag == "Destructable"){
+			Destroy(deadthing.gameObject);
+		}
+		Destroy (gameObject);
+	}
+}
