6c6226f3d71cf5cf076a68fc2608ae32445573ca
VimsyPimsy <a12vinfr@student.his.se>
2014-04-30 16:26:47 +0200

    Flum lum

---
+++ Assets/_Scripts/ParticleSwitch.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSwitch : TriggerAction {
+	
+	public float _countDown;
+
+	// Use this for initialization
+	void Start () {
+		//_countDown = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(particleSystem.loop == false){
+			_countDown -= Time.deltaTime;
+			if(_countDown <= 0){
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	public override void onActive(){
+		if (gameObject.name.Equals ("ParticleTo")) { 
+			gameObject.particleSystem.Play ();
+			gameObject.particleSystem.loop = false;
+		} else if(gameObject.name.Equals ("ParticleFrom")) {
+			gameObject.particleSystem.loop = false;
+			GetComponent<Trigger>().enabled = false;
+			GetComponent<BoxCollider>().enabled = false;
+		}
+	}
+
+	public override void onInactive(){
+
+	}
+
+}
---
+++ Assets/_Scripts/StartScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour {
+
+	Rect _startRect;
+	
+
+	void OnGUI(){
+
+		GUI.Box (new Rect (Screen.width/2.0f-(Screen.width*0.2f)/2, Screen.height*0.1f, Screen.width*0.2f, Screen.height*0.6f), "Menu");
+
+		if(GUI.Button (new Rect (Screen.width/2.0f-(Screen.width*0.2f)/2, Screen.height*0.1f, 70, 30), "Patrik")){
+			Application.LoadLevel("PatrikScene");
+		}
+
+		if(GUI.Button (new Rect (30, 90, 70, 30), "Lars")){
+			Application.LoadLevel("LurzScene");
+		}
+
+	}
+}
