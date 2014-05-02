19d3b6c2cdba8c867d3047fa0caedbffef756e36
leejesslee <jessicall_2@msn.com>
2014-05-02 10:39:30 +0200

    :D

--- Assets/_Scripts/PushAndPull.cs
+++ Assets/_Scripts/PushAndPull.cs
@@ -24,7 +24,8 @@ public class PushAndPull : MonoBehaviour {
 	float _distance;
 	Vector3 _direction;
 	RaycastHit _derp;
-	bool _collided = false;
+	bool _collidedf = false;
+	bool _collidedb = false;
 
 	//Vector3 _herpaderp;
 	
@@ -42,184 +43,59 @@ public class PushAndPull : MonoBehaviour {
 			else{
 				_speed = 0;
 			}
-			
-		/*	_deltapos = transform.position - _position;
-			_deltaobjpos = _obj.position - _objpos;
-
-			/*
-			if(_hasMoved && Mathf.Abs(_deltaobjpos.x)< float.Epsilon && Mathf.Abs(_deltapos.z)< float.Epsilon){
-				_boystate.ActivateWalk();
-			}
-
-
-			_hasMoved = false;
-			*/
-		/*	if(Mathf.Abs(_deltapos.x) > 0 || Mathf.Abs(_deltapos.z) >0){
-				//_obj.rigidbody.MovePosition(_obj.position + new Vector3(_deltapos.x,0,_deltapos.z));
-				//_hasMoved = true;
-			}
-			
-			if(_cubecol.getCollision()){
-				if(_speed>0){
-					_blockedForward = true;
-					//_cubecol.deactivateCollision();
-				}
-				else if(_speed<0){
-					_blockedBackwards = true;
-					//_cubecol.deactivateCollision();
-				}
-				_speed = 0;
-				//Vector3 temp = _obj.position;
-				_obj.position = _cubecol._lastPos;
-				_cubecol.deactivateCollision();
-				//transform.position += _obj.position - temp;
-				Vector3 _objdir = _obj.TransformDirection(transform.forward);
-				float _objside;
-				if(Mathf.Abs(_objdir.x) > Mathf.Abs(_objdir.z)){
-					_objside = (_obj.collider as BoxCollider).size.x;
-				}
-				else{
-					_objside = (_obj.collider as BoxCollider).size.z;
-				}
-				_distance = ((_objside/2) + _offset);
-				transform.position = new Vector3(_obj.position.x,transform.position.y,_obj.position.z) + _distance*_direction;
-
-			}
-			if(_blockedForward){
-				if( _speed < 0){
-					_blockedForward = false;
-				}
-				else if(_speed > 0){
-					_speed = 0;
-					_obj.position = _cubecol._lastPos;
-					Vector3 _objdir = _obj.TransformDirection(transform.forward);
-					float _objside;
-					if(Mathf.Abs(_objdir.x) > Mathf.Abs(_objdir.z)){
-						_objside = (_obj.collider as BoxCollider).size.x;
-					}
-					else{
-						_objside = (_obj.collider as BoxCollider).size.z;
-					}
-					_distance = ((_objside/2) + _offset);
-					transform.position = new Vector3(_obj.position.x,transform.position.y,_obj.position.z) + _distance*_direction;
-				}
-				else{
-					_obj.position = _cubecol._lastPos;
-					Vector3 _objdir = _obj.TransformDirection(transform.forward);
-					float _objside;
-					if(Mathf.Abs(_objdir.x) > Mathf.Abs(_objdir.z)){
-						_objside = (_obj.collider as BoxCollider).size.x;
-					}
-					else{
-						_objside = (_obj.collider as BoxCollider).size.z;
-					}
-					_distance = ((_objside/2) + _offset);
-					transform.position = new Vector3(_obj.position.x,transform.position.y,_obj.position.z) + _distance*_direction;
-				}
-			}
-			*/
-			//if((_blockedForward && _speed>0) || (_blockedBackwards && _speed<0)){
-				//_speed=0;
-			//}
-
-			//if(Mathf.Abs(_obj.position.y - _objpos.y)>0.05){
-
-				//_boystate.ActivateWalk();
-			//}
-			/*
-			if(_blockedBackwards){
-				if( _speed > 0){
-					_blockedBackwards = false;
-				}
-				else if(_speed < 0){
-					_obj.position = _cubecol._lastPos;
-					_boystate.ActivateWalk();
-					_blockedForward = false;
-					_blockedBackwards = false;
-					Vector3 _objdir = _obj.TransformDirection(transform.forward);
-					float _objside;
-					if(Mathf.Abs(_objdir.x) > Mathf.Abs(_objdir.z)){
-						_objside = (_obj.collider as BoxCollider).size.x;
-					}
-					else{
-						_objside = (_obj.collider as BoxCollider).size.z;
-					}
-					_distance = ((_objside/2) + _offset);
-					transform.position = new Vector3(_obj.position.x,transform.position.y,_obj.position.z) + _distance*_direction;
-				}
-				else{
-					_obj.position = _cubecol._lastPos;
-					Vector3 _objdir = _obj.TransformDirection(transform.forward);
-					float _objside;
-					if(Mathf.Abs(_objdir.x) > Mathf.Abs(_objdir.z)){
-						_objside = (_obj.collider as BoxCollider).size.x;
-					}
-					else{
-						_objside = (_obj.collider as BoxCollider).size.z;
-					}
-					_distance = ((_objside/2) + _offset);
-					transform.position = new Vector3(_obj.position.x,transform.position.y,_obj.position.z) + _distance*_direction;
-				}
-			}*/
-			//if(_speed != 0){
-			if(!_obj.rigidbody.SweepTest(-_direction*Mathf.Sign(_speed), out _derp, 0.05f)){
-				_collided = false;
-				Debug.Log("Skriv inte ut detta plz");
-				_position = transform.position;
-			}
-			else{
-			//	_obj.rigidbody.MovePosition(new Vector3(transform.position.x,_objposy,transform.position.z) + _distance*_direction*-1);
-				if(!_collided){
+			if(_obj.rigidbody.SweepTest(-_direction, out _derp, 0.1f)){
+				if(!_collidedf){
 					if(_speed > 0){
 						_blockedForward = true;
+						_speed = 0;
 					}
-					else if(_speed < 0){
-						_blockedBackwards = true;
-					}
-					_speed = 0;
 				}
-				if(_collided){
+				else if(_collidedf){
 					if(_blockedForward){
 						if(_speed>0){
 							_speed = 0;
 						}
 						else if(_speed<0){
 							_blockedForward = false;
-							//_obj.rigidbody.MovePosition(new Vector3(transform.position.x,_objposy,transform.position.z) + _distance*_direction*-1);
 						}
 					}
-					else if(_blockedBackwards){
+				}
+				_collidedf = true;
+				Debug.Log("THIS MOTHAFUCKA COLLIDED");
+			}
+			else if(_obj.rigidbody.SweepTest(_direction, out _derp, 0.1f)){
+				if(!_collidedb){
+					if(_speed < 0){
+						_blockedBackwards = true;
+						_speed = 0;
+					}
+				}
+				else if(_collidedb){
+					if(_blockedBackwards){
 						if(_speed<0){
 							_speed = 0;
 						}
 						else if(_speed>0){
 							_blockedBackwards = false;
-							//_obj.rigidbody.MovePosition(new Vector3(transform.position.x,_objposy,transform.position.z) + _distance*_direction*-1);
 						}
 					}
 				}
-				_collided = true;
-				Debug.Log("THIS MOTHAFUCKA COLLIDED");
+				_collidedb = true;
 			}
-
-				//_obj.rigidbody.MovePosition(new Vector3(transform.position.x,_objposy,transform.position.z) + _distance*_direction*-1);
-				//_obj.rigidbody.MovePosition(new Vector3(_ani.bodyPosition.x,_objposy,_ani.bodyPosition.z) + _distance*_direction*-1);
-				//_herpaderp = _ani.bodyPosition;
-			//}else{
-				//_ani.bodyPosition = _herpaderp;
-			//}
+			else{
+				_collidedf = false;
+				_collidedb = false;
+			}
 
 			if(_speed == 0){
 				transform.position = _position;
 			}
-
+	
 			_ani.SetFloat("Speed", _speed);
-			_obj.rigidbody.MovePosition(new Vector3(transform.position.x,_objposy,transform.position.z) + _distance*_direction*-1);
-			Debug.Log("Collided is "+_collided);
-			//_obj.rigidbody.MovePosition(new Vector3(transform.position.x,_objposy,transform.position.z) + _distance*_direction*-1);
-
 			transform.forward = _direction * -1;
-			transform.localPosition = new Vector3(_boyposx, transform.localPosition.y, transform.localPosition.z);
+			transform.position = new Vector3(_position.x, transform.localPosition.y, transform.localPosition.z);
+			_obj.rigidbody.MovePosition(new Vector3(transform.position.x,_objposy,transform.position.z) + _distance*_direction*-1);
+			_position = transform.position;
 		}
 	}
 
@@ -252,7 +128,7 @@ public class PushAndPull : MonoBehaviour {
 			_ani.SetBool("Pushing",true);
 			//_cubecol = _obj.GetComponent<CubeCollision>();
 
-			_boyposx = transform.localPosition.x;
+			_position = transform.position;
 		}
 		else{
 			_ani.SetBool("Pushing",false);
