518247b9df9a359b9461fa5be0d50a64169c9ea6
Patah93 <hedenstrom.patrik@gmail.com>
2014-05-02 12:12:51 +0200

    Skugg

--- Assets/_Scripts/Fox&Shadows/ShadowDetection.cs
+++ Assets/_Scripts/Fox&Shadows/ShadowDetection.cs
@@ -281,6 +281,13 @@ public class ShadowDetection : MonoBehaviour {
 				_numberLightedVertices++;
 			}
 
+			/* SpotLights */
+			else if(isPointInSpotLight(/*getLamps()*/ spotLights, _pointsOfInterest[i], ref shadowCasters)){
+				//return true;
+				return_value = true;
+				_numberLightedVertices++;
+			}
+
 			/* Lampor */
 			else if(isPointInLampLight(/*getLamps()*/ lamps, _pointsOfInterest[i], ref shadowCasters)){
 				//return true;
@@ -288,12 +295,6 @@ public class ShadowDetection : MonoBehaviour {
 				_numberLightedVertices++;
 			}
 
-			/* SpotLights */
-			else if(isPointInSpotLight(/*getLamps()*/ spotLights, _pointsOfInterest[i], ref shadowCasters)){
-				//return true;
-				return_value = true;
-				_numberLightedVertices++;
-			}
 		}
 
 		//return false;
