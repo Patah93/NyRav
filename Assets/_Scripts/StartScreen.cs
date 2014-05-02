eaba2a5b13d13ff2b9e51d157a04a4ced8219b9b
Jessica Lee <jessicall_2@msn.com>
2014-05-02 11:24:41 +0200

    Meny

--- Assets/_Scripts/StartScreen.cs
+++ Assets/_Scripts/StartScreen.cs
@@ -3,20 +3,23 @@ using System.Collections;
 
 public class StartScreen : MonoBehaviour {
 
-	Rect _startRect;
-	
+	bool _isFullscreen;
 
 	void OnGUI(){
 
-		GUI.Box (new Rect (Screen.width/2.0f-(Screen.width*0.2f)/2, Screen.height*0.1f, Screen.width*0.2f, Screen.height*0.6f), "Menu");
+		//GUI.Box (new Rect (Screen.width/2.0f-(Screen.width*0.2f)/2, Screen.height*0.1f, Screen.width*0.2f, Screen.height*0.6f), "Menu");
 
-		if(GUI.Button (new Rect (Screen.width/2.0f-(Screen.width*0.2f)/2, Screen.height*0.1f, 70, 30), "Patrik")){
-			Application.LoadLevel("PatrikScene");
+		if(GUI.Button (new Rect (Screen.width/2.0f-(Screen.width*0.2f)/2, Screen.height*0.1f, 250, 30), "New Game")){
+			Application.LoadLevel("PimsScene");
 		}
 
-		if(GUI.Button (new Rect (30, 90, 70, 30), "Lars")){
-			Application.LoadLevel("LurzScene");
+		if(GUI.Button (new Rect (Screen.width/2.0f-(Screen.width*0.2f)/2, Screen.height*0.2f, 250, 30), "Options")){
+			gameObject.GetComponent<OptionsScreen>().enabled = true;
+			gameObject.GetComponent<StartScreen>().enabled = false;
 		}
 
+		if(GUI.Button (new Rect (Screen.width/2.0f-(Screen.width*0.2f)/2, Screen.height*0.3f, 250, 30), "Exit")){
+			Application.Quit();
+		}
 	}
 }
