5562e8bfec49793f9dba022bb12fa9874852b975
Vincent Friberg <a12vinfr@student.his.se>
2014-05-08 17:53:10 +0200

    Eleveter

---
+++ Assets/_Scripts/StickyMan.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickyMan : MonoBehaviour {
+		
+	bool _OnPlatform;
+	Collider _Thing;
+	private Vector3 tempPlat = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		_OnPlatform = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(_OnPlatform){
+			_Thing.transform.position += (transform.position - tempPlat);
+
+
+		}
+
+		tempPlat = transform.position;
+
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		_OnPlatform = true;
+		_Thing = other;
+		//Vector3 tempScale = other.transform.localScale;
+		//other.transform.parent = transform;
+	}
+
+	void OnTriggerExit(Collider other) {
+		_OnPlatform = false;
+		//other.transform.parent = null;
+	}
+
+}
